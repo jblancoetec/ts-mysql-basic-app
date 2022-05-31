5d9aad8b54f96cab955d29db36c1b39bbeeb074c
joackob <89130929+jblancoetec@users.noreply.github.com>
2022-05-31 09:38:06 -0300

    update connect bdd

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,11 +1,12 @@
 import mysql from "mysql";
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.MYSQLPASS || "",
-  insecureAuth: true,
-});
+const config: mysql.ConnectionConfig = {
+  host: process.env.BDD_HOST,
+  user: process.env.BDD_USER,
+  password: process.env.BDD_PASS,
+};
+
+const connection = mysql.createConnection(config);
 
 connection.connect((err: any) => {
   console.log(err ? err.stack : "Connected!");
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.MYSQLPASS);
+console.log(process.env.BDD_PASS);
 import "./database/connect";
 
 console.log("Good looking in your hacking space!");
